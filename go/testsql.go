d8d39543b75a7ca55fd4b940fb77d0229d01e914
yxsicd <yxsicd@users.noreply.github.com>
2018-04-02 04:58:55 +0800

    Update testsql.go

--- go/testsql.go
+++ go/testsql.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 )
 
@@ -48,18 +49,17 @@ func sendsql(db, sql string) {
 		return
 	}
 	all_ret_len = int64(len(retb)) + all_ret_len
-	// log.Printf("ret is %s", retb)
+	log.Printf("ret is %s", retb)
 }
 
-func main() {
+func batchsql(sql string, t_count int) {
 	begin_time := time.Now()
-	t_count := 30000
 	done := make(chan int, t_count)
-	p_count := make(chan int, 128)
+	p_count := make(chan int, 256)
 	for i := 0; i < t_count; i++ {
 		go func() {
 			p_count <- 1
-			sendsql("test.db", "select * from people;")
+			sendsql("test.db", sql)
 			<-p_count
 			done <- 1
 		}()
@@ -71,5 +71,17 @@ func main() {
 	use_time := time.Since(begin_time)
 	speed := float64(t_count) / (float64(use_time.Nanoseconds()) / 1000000000)
 	log.Printf("use time is %v, speed is %v, all_ret_content is %v, one requeset use time is %v",
-		use_time, speed, all_ret_len/1024/1024, use_time.Nanoseconds()/int64(t_count))
+		use_time, speed, all_ret_len/1024, use_time.Nanoseconds()/int64(t_count))
+}
+
+func main() {
+
+	var batch_sql []string
+	for i := 0; i < 1; i++ {
+		batch_sql = append(batch_sql, "insert into people select 'n1','123123123';")
+	}
+
+	batchsql(strings.Join(batch_sql, " "), 5)
+	batchsql("select count(1) from people;", 10)
+
 }
