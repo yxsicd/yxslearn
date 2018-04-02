2c2efbc89fcea51b7f61244b39a7d1ab04009db9
yxsicd <yxsicd@users.noreply.github.com>
2018-04-03 07:11:16 +0800

    Update testsql.go

--- go/testsql.go
+++ go/testsql.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
-	"strings"
 	"time"
 )
 
@@ -34,10 +34,16 @@ for (var i = 0; i < 100000; i++) {
 
 var all_ret_len int64
 
-func sendsql(db, sql string) {
+func sendsql(db, sql string, data [][]string, show bool) {
 	esql := url.QueryEscape(sql)
 	edb := url.QueryEscape(db)
-	ret, err := http.Get(fmt.Sprintf("http://localhost:8080/?db=%s&sql=%s", edb, esql))
+
+	ejsondata := ""
+	if data != nil {
+		jsondata, _ := json.Marshal(data)
+		ejsondata = url.QueryEscape(string(jsondata))
+	}
+	ret, err := http.Get(fmt.Sprintf("http://localhost:8080/?db=%s&sql=%s&data=%s", edb, esql, ejsondata))
 	if err != nil {
 		log.Printf("send sql err is %s", err)
 		return
@@ -49,17 +55,23 @@ func sendsql(db, sql string) {
 		return
 	}
 	all_ret_len = int64(len(retb)) + all_ret_len
-	log.Printf("ret is %s", retb)
+	if show {
+		log.Printf("run sql %s ret  is %s", sql, retb)
+	}
 }
 
-func batchsql(sql string, t_count int) {
+func batchsql(sql string, data [][]string, t_count int) {
+	batchsqlshow(sql, data, t_count, false)
+}
+
+func batchsqlshow(sql string, data [][]string, t_count int, show bool) {
 	begin_time := time.Now()
 	done := make(chan int, t_count)
 	p_count := make(chan int, 256)
 	for i := 0; i < t_count; i++ {
 		go func() {
 			p_count <- 1
-			sendsql("test.db", sql)
+			sendsql("test.db", sql, data, show)
 			<-p_count
 			done <- 1
 		}()
@@ -70,18 +82,28 @@ func batchsql(sql string, t_count int) {
 
 	use_time := time.Since(begin_time)
 	speed := float64(t_count) / (float64(use_time.Nanoseconds()) / 1000000000)
-	log.Printf("use time is %v, speed is %v, all_ret_content is %v, one requeset use time is %v",
+	log.Printf("run %s use time is %v, speed is %v, all_ret_content is %v, one requeset use time is %v", sql,
 		use_time, speed, all_ret_len/1024, use_time.Nanoseconds()/int64(t_count))
 }
 
 func main() {
 
 	var batch_sql []string
-	for i := 0; i < 1; i++ {
+	for i := 0; i < 300; i++ {
 		batch_sql = append(batch_sql, "insert into people select 'n1','123123123';")
 	}
+	batchsql("create table if not exists people(name,age);", nil, 1)
+	// batchsql(strings.Join(batch_sql, " "), 10)
 
-	batchsql(strings.Join(batch_sql, " "), 5)
-	batchsql("select count(1) from people;", 10)
+	var batch_insert_data [][]string
+	for i := 0; i < 2000; i++ {
+		var insert_row []string
+		insert_row = append(insert_row, "n1", "a1")
+		batch_insert_data = append(batch_insert_data, insert_row)
+	}
+	batchsql("insert into people values(?,?);", batch_insert_data, 10)
+	batchsql("select * from people limit 1;", nil, 1000)
+	// batchsql("select count(1) from people ;", nil, 1000)
+	batchsqlshow("select count(1) from people ;", nil, 1, true)
 
 }
